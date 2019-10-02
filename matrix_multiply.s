a749d105f10560ed30affc16b5f72d9749cade64
feeerr95 <feeerr95@gmail.com>
2019-10-02 16:03:11 -0300

    Update de matrix_multiply.s

--- matrix_multiply.s
+++ matrix_multiply.s
@@ -45,8 +45,9 @@ matrix_multiply:
 
 	beqz	s0, _created_matrix_error
 
-	lw 		t0, 40($fp) #guardo a0 de nuevo en t0
-	lw		t1, 0(t0) #m1->rows = N
+	lw 		a0, 40($fp) #cargo a0 (m1)
+	lw		t0, 0(a0) #t1 = m1->rows = N
+	lw 		a1, 44($fp) #cargo a1 (m2)
 
 	move	t2, zero # i=0: t2
 for_i:
@@ -54,29 +55,45 @@ for_i:
 	move	t3, zero # x=0: t3
 for_x:
 
-	sw 		t6, 8(s0) #array de mresult
-
-	# aca hay que hacer esto: mresult->array[i*mresult->rows + x] = value;
-
-	la 		t9, index_value
-	jal 	t9
+	lw 		t6, 8(s0) #t6 = mresult->array
+	lw      t7, 0(s0) #t7 = mresult->rows
+	mul 	t7, t7, t2 #t7 = mresult->rows * i
+	addu 	t7, t7, t3 #t7 = mresult->rows * i + x
+	sw 		zero, t6(t7) # mresult->array[t7] = 0
 
 	move	t4, zero # y=0: t4
 for_y:
-	#double value = value_obtain(mresult,x,i);
-	#value += value_obtain(m1,y, i) * value_obtain(m2,x,y);
-	#index_value(mresult,x,i,value);
+	
+	lw 		t6, 8(a0) #t6 = m1->array
+	lw      t7, 0(a0) #t7 = m1->rows
+	mul 	t7, t7, t2 #t7 = m1->rows * i
+	addu 	t7, t7, t4 #t7 = m1->rows * i + x
+	l.d 	f0, t6(t7) #creo que en f0 se guarda m1->array[i*m1->rows + y]
+
+	lw 		t6, 8(a1) #t6 = m2->array
+	lw      t7, 0(a1) #t7 = m2->rows
+	mul 	t7, t7, t2 #t7 = m2->rows * i
+	addu 	t7, t7, t4 #t7 = m2->rows * i + x
+	l.d 	f1, t6(t7) #creo que en f1 se guarda m2->array[i*m1->rows + y]
+
+	mul.s 	f0, f1, f0 # multiplcacion de valores de las matrices
+
+	lw 		t6, 8(s0) #t6 = mresult->array
+	lw      t7, 0(s0) #t7 = mresult->rows
+	mul 	t7, t7, t2 #t7 = mresult->rows * i
+	addu 	t7, t7, t3 #t7 = mresult->rows * i + x
+	s.d		f0, t6(t7) # mresult->array[t7] = 0
 	
 	addiu 	t4, t4, 1 # y++
-	sltu	t5, t1, t4 # if t1 < t4 (N < y) t5 = 1 else t5 = 0
+	sltu	t5, t0, t4 # if t0 < t4 (N < y) t5 = 1 else t5 = 0
 	beqz 	t5, for_y 
 
 	addiu 	t3, t3, 1 # x++
-	sltu	t5, t1, t3
+	sltu	t5, t0, t3
 	beqz 	t5, for_x
 
 	addiu 	t2, t2, 1 #i++
-	sltu	t5, t1, t2
+	sltu	t5, t0, t2
 	beqz 	t5, for_i 
 
 	#hay que hacer el return mresult;
@@ -124,7 +141,7 @@ matrix_t* matrix_multiply(matrix_t* m1, matrix_t* m2){
 	int N = m1->cols;
 	for(int i = 0; i < N; i++){
 	 	for(int x = 0; x < N; x++){
-	        mresult->array[i*mresult->rows + x] = value;
+	        mresult->array[i*mresult->rows + x] = 0;
 	 		for(int y = 0; y < N; y++){
 	 			double value = value_obtain(mresult,x,i);
 	 			value += value_obtain(m1,y, i) * value_obtain(m2,x,y);
